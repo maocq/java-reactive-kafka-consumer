7d49a2dbc76244bf4e121f152ca5393cb825f135
maocq <johncarm@bancolombia.com.co>
2024-08-03 16:46:02 -0500

    Init

---
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/bancolombia/api/Handler.java
@@ -0,0 +1,26 @@
+package co.com.bancolombia.api;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+@Component
+@RequiredArgsConstructor
+public class Handler {
+//private  final UseCase useCase;
+
+    public Mono<ServerResponse> listenGETUseCase(ServerRequest serverRequest) {
+        // usecase.logic();
+        return ServerResponse.ok().bodyValue("Hello");
+    }
+
+    public Mono<ServerResponse> listenGETOtherUseCase(ServerRequest serverRequest) {
+        return ServerResponse.ok().bodyValue("");
+    }
+
+    public Mono<ServerResponse> listenPOSTUseCase(ServerRequest serverRequest) {
+        return ServerResponse.ok().bodyValue("");
+    }
+}
